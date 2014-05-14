278d10dfc32e72748367d53bd0ed85665159c33b
Alexander Petkov <abpetkov@gmail.com>
2014-05-14 15:30:12 +0300

    Update standalone to 0.5.5

--- dist/switchery.js
+++ dist/switchery.js
@@ -1059,7 +1059,7 @@ if (typeof define !== 'undefined' && define.amd) {
 });
 require.register("switchery/switchery.js", function(exports, require, module){
 /**
- * Switchery 0.5.4
+ * Switchery 0.5.5
  * http://abpetkov.github.io/switchery/
  *
  * Authored by Alexander Petkov
